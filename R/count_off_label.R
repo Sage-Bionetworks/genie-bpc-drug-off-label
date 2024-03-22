9fed0125940ee1362fd10f9b030aabd970acfc4d
alexpaynter <alexcpaynter@gmail.com>
2024-03-21 22:52:58 -0700

    display retooling

--- R/count_off_label.R
+++ R/count_off_label.R
@@ -1,11 +1,12 @@
 count_off_label <- function(
-  dat_det
+  dat_det,
+  group_vars = c('cohort', 'agent')
 ) {
   dat_det %>%
-    group_by(cohort, agent) %>%
+    group_by(across(all_of(group_vars))) %>%
     summarize(
       n_uses = n(),
-      n_on_label = sum(ind_exists, na.rm = T),
+      n_on_label = sum(valid_ind_exists, na.rm = T),
       n_off_label = n_uses - n_on_label,
       prop_off_label = n_off_label / n_uses, 
       .groups = "drop"

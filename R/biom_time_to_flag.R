32126b38b1657f9e7c3d26879d06da6482cfd44f
alexpaynter <alexcpaynter@gmail.com>
2024-11-15 07:36:48 -0800

    Extremely impactful </> error.

--- R/biom_time_to_flag.R
+++ R/biom_time_to_flag.R
@@ -6,7 +6,7 @@ biom_time_to_flag <- function(
   # half day tolerance around all
   rtn <- case_when(
     is.na(tt_ref) | is.na(tt_test) ~ NA_real_, # should never happen with drugs
-    tt_ref > tt_test ~ NA_real_, # not tested at ref time
+    tt_ref < tt_test ~ NA_real_, # not tested at ref time
     is.na(tt_pos) ~ 0,
     tt_ref >= tt_pos ~ 1,
     tt_ref < tt_pos ~ 0,
